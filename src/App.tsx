a37c5c7a80f89020c4bf0e64bf7a7b6a9f58f97b
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-04-18 13:26:40 +0530

    update: init project structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { Button } from 'antd';
+
+const App: FC = () => (
+	<div className="App">
+		<Button type="primary">Button</Button>
+	</div>
+);
+
+export default App;
